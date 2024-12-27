3580280a296d73adcd5d4de81cb6508bed202eaf
Nakesh_Tewari <nakesh1107@gmail.com>
2024-12-27 18:44:48 +0530

    commit

--- app.js
+++ app.js
@@ -2,25 +2,25 @@ const express = require("express");
 const app = express();
 const router = require("./routes/router");
 const cors = require("cors");
-<<<<<<< HEAD
+
 const dotenv = require("dotenv");
-=======
+
 const dotenv=require("dotenv");
->>>>>>> 109cd982fdf8c973a17af54f754e281363e52c31
+
 
 dotenv.config();
 
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
-<<<<<<< HEAD
+
 app.use(cors());
 
-=======
+
 app.use(
   cors()
 );
->>>>>>> 109cd982fdf8c973a17af54f754e281363e52c31
+
 app.use(router);
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
